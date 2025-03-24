d1d0b0a518404c168e3b9bcd89456e73298b6abb
0xc0debabe <kongminoo@naver.com>
2025-03-24 13:17:44 +0900

    first commit

---
+++ src/main/java/com/diary/private_diary/PrivateDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.diary.private_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diary/private_diary/PrivateDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.private_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
