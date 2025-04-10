fc31d13ca7e5ce173f17a85a39e8faccf20893a9
0xc0debabe <kongminoo@naver.com>
2025-04-10 19:10:56 +0900

    feat: jwt 구현

---
+++ src/main/java/com/hnc/mogak/global/auth/jwt/CustomUserDetails.java
@@ -0,0 +1,42 @@
+package com.hnc.mogak.global.auth.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+class CustomUserDetails implements UserDetails {
+
+    private String providerId;
+    private String nickname;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return role;
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return providerId;
+    }
+
+}
---
+++ src/main/java/com/hnc/mogak/global/auth/jwt/JwtFilter.java
@@ -0,0 +1,49 @@
+package com.hnc.mogak.global.auth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.hnc.mogak.global.auth.AuthConstant.AUTHORIZATION;
+import static com.hnc.mogak.global.auth.AuthConstant.BEARER;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader(AUTHORIZATION);
+        if (authorization == null || !authorization.startsWith(BEARER)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String accessToken = authorization.substring(BEARER.length()).trim();
+        if (jwtUtil.isTokenExpired(accessToken)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String findProviderId = jwtUtil.getProviderId(accessToken);
+        String findNickname = jwtUtil.getNickname(accessToken);
+        String findRole = jwtUtil.getRole(accessToken);
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(findProviderId, findNickname, findRole);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+
+}
