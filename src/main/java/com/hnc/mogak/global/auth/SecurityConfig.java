cb5e569d35acebc7967bfbb3f42b3cb7fb84431a
0xc0debabe <kongminoo@naver.com>
2025-06-23 16:46:54 +0900

    refactor : CORS 경로 수정

--- src/main/java/com/hnc/mogak/global/auth/SecurityConfig.java
+++ src/main/java/com/hnc/mogak/global/auth/SecurityConfig.java
@@ -89,6 +89,7 @@ public class SecurityConfig {
                                         "/swagger-ui.html",
                                         "/dashboard/**",
                                         "/webjars/**",
+                                        "/error",
                                         "/actuator/**"
                                 ).permitAll()
                                 .anyRequest().authenticated())
@@ -116,7 +117,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 주소
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://www.mogak.kr"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
