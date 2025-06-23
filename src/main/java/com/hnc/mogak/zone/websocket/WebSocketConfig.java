7994ec97f0dbf5f611c99220c23c9a18593e1cf2
0xc0debabe <kongminoo@naver.com>
2025-06-23 17:08:57 +0900

    refactor : CORS 경로 수정3

--- src/main/java/com/hnc/mogak/zone/websocket/WebSocketConfig.java
+++ src/main/java/com/hnc/mogak/zone/websocket/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/api/mogak/ws")
                 .setAllowedOrigins("https://www.mogak.kr")
                 .withSockJS();
     }
