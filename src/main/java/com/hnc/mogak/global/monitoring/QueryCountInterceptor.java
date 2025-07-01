b604431ef1b0831ab518d97644759a444fb712dd
0xc0debabe <kongminoo@naver.com>
2025-07-01 20:46:09 +0900

    refactor : 로그 추가 12

--- src/main/java/com/hnc/mogak/global/monitoring/QueryCountInterceptor.java
+++ src/main/java/com/hnc/mogak/global/monitoring/QueryCountInterceptor.java
@@ -38,7 +38,7 @@ public class QueryCountInterceptor implements HandlerInterceptor {
 
         RequestContextHolder.initContext(ctx);
 
-        log.info("===테스트 시작===");
+        log.info("===1테스트 시작1===");
         String xff = String.valueOf(request.getHeader("X-Forwarded-For"));
         log.info("XFF: {}", xff);
         log.info("[{}] Request Method=[{}]", ctx.getUuid(), request.getMethod());
